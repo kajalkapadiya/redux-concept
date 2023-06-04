af3119d468f234960fdfae0348c9df2bc16fa38c
kajalkapadiya <kajalkapadiya18@gmail.com>
2023-06-04 16:18:26 +0530

    learning redux

---
+++ redux.js
@@ -0,0 +1,25 @@
+const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "incrementby2") {
+    return {
+      counter: state.counter + 2,
+    };
+  } else if (action.type === "decrementby2") {
+    return {
+      counter: state.counter - 2,
+    };
+  }
+};
+
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "incrementby2" });
+store.dispatch({ type: "decrementby2" });
